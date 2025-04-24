186a4b34afc64008a49c3051bf1fd2939fa7bbe5
Dennis Furtkevic <94914222+DovarFalcone@users.noreply.github.com>
2025-04-24 15:00:11 -0400

    Update cpu_temp.sh

--- pizero/cpu_temp.sh
+++ pizero/cpu_temp.sh
@@ -4,13 +4,15 @@
 #* * * * * /home/dennis/sensor_projects/temperature/cpu_temp.sh
 
 # Replace these with your actual values
+HOST="your_host"
+PORT="host_port"
 TOKEN="your_influxdb_token"
 ORG="your_org"
-BUCKET="raspberrypi"
+BUCKET="your_bucket"
 
 TEMP=$(</sys/class/thermal/thermal_zone0/temp)
 TEMP_C=$(echo "scale=2; $TEMP / 1000" | bc)
 
-curl -XPOST "http://10.0.0.120:8086/api/v2/write?org=$ORG&bucket=$BUCKET&precision=s" \
+curl -XPOST "http://$HOST:$PORT/api/v2/write?org=$ORG&bucket=$BUCKET&precision=s" \
   --header "Authorization: Token $TOKEN" \
   --data-binary "cpu_temp,host=pi_zero2w value=$TEMP_C"
